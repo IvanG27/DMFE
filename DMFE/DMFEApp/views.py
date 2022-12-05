b803fd318e022b30aed814db7a39ceef46e8b916
IvanG27 <ivango28215@gmail.com>
2022-12-05 09:05:45 -0600

    Mostrar datos
    Mostramos los 10 primeros datos del csv para verificar que sea el archivo

--- DMFE/DMFEApp/views.py
+++ DMFE/DMFEApp/views.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np                     
 import matplotlib.pyplot as plt        
 import seaborn as sns
-import cv2
 
 # Create your views here.
 def home(request):
@@ -27,12 +26,34 @@ def mostrarDatos(request, id_file):
     file = models.Document.objects.get(pk=id_file)
     archivo = pd.read_csv(file.uploadedFile)
     Correlacion = archivo.corr(method='pearson')
-    archivo = archivo.to_html
+    
     plt.figure(figsize=(14,7))
     MatrizInf = np.triu(Correlacion)
     sns.heatmap(Correlacion, cmap='RdBu_r', annot=True, mask=MatrizInf)
     plt.savefig('media/prueba3.png')
-    context = {'archivo': archivo}  
+
+    columns = "<tr>"
+    for col in archivo.columns:
+        columns += f"<th>{col}</th>"
+    columns += "</tr>"
+
+    hola = "<tr>"
+    for col in archivo.columns:
+        hola+= f"{col}"
+    hola += "adios"
+
+    rows = ""
+    iter = 0
+    for row in archivo.values:
+        rows += "<tr>"
+        for value in row:
+            rows += f"<td>{value}</td>"
+        rows += "</tr>"
+        iter+=1
+        if iter>9:
+            break
+    files = models.Document.objects.all()
+    context = {"columns": columns, "rows": rows, "files":files}  
     return render(request, "DMFEApp/PCA.html", context)
 
 def uploadFile(request):
